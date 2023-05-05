a5e642cc7f994b08b3244c6309e4d94c90ec103a
goncalves-ac <goncalves.ac@outlook.com>
2023-05-05 15:24:26 -0300

    Iniciando a aula 3 e aplicando algumas correções que foram necessárias para passar nos testes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,22 +13,22 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self): 
     
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text  
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a entrar com um item TODO imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')  
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
@@ -41,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element(By.ID,'id_new_item')  
         inputbox.send_keys('Estudar testes de unidade')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
