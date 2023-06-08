7dae05df576462b5e3ec8910cb0404f72e6ecd0d
goncalves-ac <goncalves.ac@outlook.com>
2023-06-07 22:44:37 -0300

    Validando os erros na view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.utils.html import escape
+
 
 class HomePageTest(TestCase):
 
@@ -66,6 +68,13 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+        
 
 class NewItemTest(TestCase):
 
@@ -94,4 +103,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
-        
